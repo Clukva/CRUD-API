4f301cb2063a9e3383225c9b2dbd686204366e0c
Clukva <yura4-1-1@mail.ru>
2023-07-03 21:45:05 +0300

    feat: add comment to readme file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # CRUD-API
+
+Please use for check platform for API development such Postman or another
+
+[1]
+-- Clone repository - git clone https://github.com/Clukva/CRUD-API.git
+-- go to develop folder - cd CRUD-API
+-- go to branch develop - git checkout develop
+-- install dependencies - npm i
+[2]
+
+npm run start:prod
+
+Check basic score:
+
++10 The repository with the application contains a Readme.md file containing detailed instructions for installing, running and using the application
++10 GET api/users implemented properly
++10 GET api/users/{userId} implemented properly
++10 POST api/users implemented properly
++10 PUT api/users/{userId} implemented properly
++10 DELETE api/users/{userId} implemented properly
++6 Users are stored in the form described in the technical requirements
++6 Value of port on which application is running is stored in .env file
+
+Check advanced score
+
++30 Task implemented on Typescript
++10 Processing of requests to non-existing endpoints implemented properly
++10 Errors on the server side that occur during the processing of a request should be handled and processed properly
+( uncomment code in server.ts after comment // for check server error -- for check server errors)
++10 Development mode: npm script start:dev implemented properly
++10 Production mode: npm script start:prod implemented properly
--- src/server.ts
+++ src/server.ts
@@ -12,107 +12,118 @@ export const server = http.createServer((req, res) => {
   const { method, url, headers } = req;
   let body: Uint8Array[] = [];
 
-  req.on("data", (chunk) => {
-    body.push(chunk);
-  });
+  try {
+    // for check server error
+    /* if (true) {
+      throw new Error("Custom error message");
+    } */
 
-  req.on("end", () => {
-    const requestBody = Buffer.concat(body).toString();
-    if (url === "/api/users" && method === "GET") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(users));
-    } else if (url?.startsWith("/api/users/") && method === "GET") {
-      const userId = url.split("/")[3];
-      if (isUUID(userId)) {
-        const user = users.find((user) => user.id === userId);
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
 
-        if (!user) {
-          res.writeHead(404, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ message: "Person isn't found" }));
-        } else {
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(user));
-        }
-      } else {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Invalid data in request" }));
-      }
-    } else if (url === "/api/users" && method === "POST") {
-      const { username, age, hobbies } = JSON.parse(requestBody);
+    req.on("end", () => {
+      const requestBody = Buffer.concat(body).toString();
+      if (url === "/api/users" && method === "GET") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(users));
+      } else if (url?.startsWith("/api/users/") && method === "GET") {
+        const userId = url.split("/")[3];
+        if (isUUID(userId)) {
+          const user = users.find((user) => user.id === userId);
 
-      if (!username || !age || !hobbies) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            message: "Username, age and hobbies are required fields",
-          })
-        );
-      } else {
-        const newUser: User = {
-          id: uuidv4(),
-          username,
-          age,
-          hobbies: hobbies || [],
-        };
-
-        users.push(newUser);
-        res.writeHead(201, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(newUser));
-      }
-    } else if (url?.startsWith("/api/users/") && method === "PUT") {
-      const userId = url.split("/")[3];
-      if (isUUID(userId)) {
-        const userIndex = users.findIndex((user) => user.id === userId);
-
-        if (userIndex === -1) {
-          res.writeHead(404, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ message: "Person isn't found" }));
-        } else {
-          const { username, age, hobbies } = JSON.parse(requestBody);
-          if (!username || !age || !hobbies) {
-            res.writeHead(400, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({
-                message: "Username, age and hobbies are required fields",
-              })
-            );
+          if (!user) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Person isn't found" }));
           } else {
-            const updatedUser: User = {
-              id: userId,
-              username,
-              age,
-              hobbies: hobbies || [],
-            };
-
-            users[userIndex] = updatedUser;
             res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(updatedUser));
+            res.end(JSON.stringify(user));
           }
-        }
-      } else {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Invalid data in request" }));
-      }
-    } else if (url?.startsWith("/api/users/") && method === "DELETE") {
-      const userId = url.split("/")[3];
-      if (isUUID(userId)) {
-        const userIndex = users.findIndex((user) => user.id === userId);
-
-        if (userIndex === -1) {
-          res.writeHead(404, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ message: "Person isn't found" }));
         } else {
-          users.splice(userIndex, 1);
-          res.writeHead(204, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ message: "Person was deleted" }));
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Invalid data in request" }));
+        }
+      } else if (url === "/api/users" && method === "POST") {
+        const { username, age, hobbies } = JSON.parse(requestBody);
+
+        if (!username || !age || !hobbies) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({
+              message: "Username, age and hobbies are required fields",
+            })
+          );
+        } else {
+          const newUser: User = {
+            id: uuidv4(),
+            username,
+            age,
+            hobbies: hobbies || [],
+          };
+
+          users.push(newUser);
+          res.writeHead(201, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(newUser));
+        }
+      } else if (url?.startsWith("/api/users/") && method === "PUT") {
+        const userId = url.split("/")[3];
+        if (isUUID(userId)) {
+          const userIndex = users.findIndex((user) => user.id === userId);
+
+          if (userIndex === -1) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Person isn't found" }));
+          } else {
+            const { username, age, hobbies } = JSON.parse(requestBody);
+            if (!username || !age || !hobbies) {
+              res.writeHead(400, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify({
+                  message: "Username, age and hobbies are required fields",
+                })
+              );
+            } else {
+              const updatedUser: User = {
+                id: userId,
+                username,
+                age,
+                hobbies: hobbies || [],
+              };
+
+              users[userIndex] = updatedUser;
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify(updatedUser));
+            }
+          }
+        } else {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Invalid data in request" }));
+        }
+      } else if (url?.startsWith("/api/users/") && method === "DELETE") {
+        const userId = url.split("/")[3];
+        if (isUUID(userId)) {
+          const userIndex = users.findIndex((user) => user.id === userId);
+
+          if (userIndex === -1) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Person isn't found" }));
+          } else {
+            users.splice(userIndex, 1);
+            res.writeHead(204, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Person was deleted" }));
+          }
+        } else {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Invalid data in request" }));
         }
       } else {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Invalid data in request" }));
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Endpoint not found" }));
       }
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Endpoint not found" }));
-    }
-  });
+    });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Server Error" }));
+  }
 });
